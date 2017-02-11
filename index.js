4a25f0c96c98ed684b8b39ee14cc1181a4ccaeef
Tim Jacobi <dev@tim-jacobi.com>
2017-02-11 01:47:53 +0000

    run prettier against this codebase ;)

--- index.js
+++ index.js
@@ -1,37 +1,37 @@
-require('colors')
+require("colors");
 const prettier = require("prettier");
-const jsdiff = require('diff');
-const fs = require('fs');
+const jsdiff = require("diff");
+const fs = require("fs");
 
-if(!process.argv[2]){
+if (!process.argv[2]) {
   console.error("Please specify a filename");
-  process.exit(1)
+  process.exit(1);
 }
 const [x, y, filename] = process.argv;
 
 const source = fs.readFileSync(filename).toString();
 
-let pass1 
+let pass1;
 try {
   pass1 = prettier.format(source);
-} catch(e) {
+} catch (e) {
   console.error(`Could not parse ${filename}`);
   process.exit(1);
 }
 const pass2 = prettier.format(pass1);
 
-if(pass1 !== pass2){
-  console.error(`------ ${filename} ------`)
+if (pass1 !== pass2) {
+  console.error(`------ ${filename} ------`);
   const diff = jsdiff.diffLines(pass1, pass2);
 
-  diff.forEach(function(part){
+  diff.forEach(function(part) {
     // green for additions, red for deletions
     // grey for common parts
-    const color = part.added ? 'green' : part.removed ? 'red' : 'grey';
-    if(part.added || part.removed){
+    const color = part.added ? "green" : part.removed ? "red" : "grey";
+    if (part.added || part.removed) {
       process.stderr.write(part.value[color]);
     }
   });
 
-  console.log()
+  console.log();
 }
